a6f929e8a96e8a33e200511efdacbb73f53408c7
Jochen Leinberger <leinberger@explorative-environments.net>
2015-12-30 18:15:32 +0100

    MQTT details

--- MQTT/mqtt_pub_test.sh
+++ MQTT/mqtt_pub_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-server=87.106.37.18
+server=87.10.37.1
 port=1883
 
 c=0
--- MQTT/mqtt_sub_test.sh
+++ MQTT/mqtt_sub_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-server=87.106.37.11
+server=87.10.37.11
 port=1883
 
 mosquitto_sub -h $server -p $port -t test/message/# -q 2
